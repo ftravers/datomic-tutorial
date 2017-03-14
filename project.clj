011a50ceb0b87bb23db06b4ce28718ffa156751f
Fenton Travers <fenton.travers@gmail.com>
2017-03-14 11:52:03 -0700

    Basic schema and testdata

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [com.datomic/datomic-free "0.9.5344" :exclusions [joda-time org.slf4j/slf4j-nop]]]
   :main ^:skip-aot datomic-tutorial.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/datomic_tutorial/core.clj
+++ src/datomic_tutorial/core.clj
@@ -1,7 +1,25 @@
 (ns datomic-tutorial.core
-  (:gen-class))
+  (:require [datomic.api :as d]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def db-conn (atom nil))
+
+(def db-url "datomic:free://127.0.0.1:4334/datomic-tutorial")
+
+(def schema [{:db/doc "A users email."
+              :db/id #db/id[:db.part/db]
+              :db/ident :user/email
+              :db/valueType :db.type/string
+              :db/cardinality :db.cardinality/one
+              :db.install/_attribute :db.part/db}])
+
+(def test-data
+  [{:db/id #db/id[:db.part/user -1]
+    :user/email "fenton.travers@gmail.com"}])
+
+(defn reload-dbs []
+  (d/delete-database db-url)
+  (d/create-database db-url)
+  (reset! db-conn (d/connect db-url))
+  (d/transact @db-conn schema)
+  (d/transact @db-conn test-data))
+
