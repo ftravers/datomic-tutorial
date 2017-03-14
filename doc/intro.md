384f0f46c04cc9fe8eb15465910fd17549174f1d
Fenton Travers <fenton.travers@gmail.com>
2017-03-14 10:37:56 -0700

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to datomic-tutorial
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/datomic_tutorial/core.clj
@@ -0,0 +1,7 @@
+(ns datomic-tutorial.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
