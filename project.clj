f193b36887e3657a0626f000724aed3615591a84
Fenton Travers <fenton.travers@gmail.com>
2017-03-21 11:25:17 -0700

    Basic schema/testdata insert.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [com.datomic/datomic-free "0.9.5344" :exclusions [joda-time org.slf4j/slf4j-nop]]]
+                 [com.datomic/datomic-free "0.9.5561" :exclusions [joda-time org.slf4j/slf4j-nop]]]
   :main ^:skip-aot datomic-tutorial.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
