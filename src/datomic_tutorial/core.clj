1ef41e03226317999c9d5a3edd62250a61187ecb
Fenton Travers <fenton.travers@gmail.com>
2017-03-27 09:42:55 -0700

    remove atom

--- src/datomic_tutorial/core.clj
+++ src/datomic_tutorial/core.clj
@@ -3,8 +3,6 @@
 
 (def db-url "datomic:free://127.0.0.1:4334/datomic-tutorial")
 
-(def db-conn (atom (d/connect db-url)))
-
 (def schema [{:db/doc "A users email."
               :db/ident :user/email
               :db/valueType :db.type/string
@@ -27,7 +25,6 @@
 (defn reload-dbs []
   (d/delete-database db-url)
   (d/create-database db-url)
-  (reset! db-conn (d/connect db-url))
-  (d/transact @db-conn schema)
-  (d/transact @db-conn test-data))
+  (d/transact (d/connect db-url) schema)
+  (d/transact (d/connect db-url) test-data))
 
